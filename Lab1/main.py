fb7f4a02793f244ca89fc28d7985b629e7dbb23d
Tamplier322 <slavashishcov1@mail.ru>
2023-02-22 16:46:45 +0300

    Adding function version 1.0

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,1 +1,13 @@
 print("Hello world!")
+
+print("Type first number: ")
+a = int(input())
+print("Type second number: ")
+b = int(input())
+print("Type operation: ")
+oper = input()
+
+def simple_calc(a, b, oper):
+    return a+b
+
+simple_calc(a, b, oper)
